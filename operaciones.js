894f8089833435da41a298236e24f7e628eb4908
GriseldaAlonso <gri_alonso@hotmail.com>
2023-11-09 20:15:19 -0500

    exportando funciones

--- operaciones.js
+++ operaciones.js
@@ -13,3 +13,10 @@ function multiplicacion(numero1, numero2){
 function division(numero1, numero2){
 	return numero1 / numero2;
 }
+
+export default {
+    suma:suma,
+    resta:resta,
+    multiplicacion:multiplicacion,
+    division:division
+}
